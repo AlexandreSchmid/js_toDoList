a046103c68d46f3a167125ff88705dfaef90dc55
AlexandreSchmid <alexandre.schmid@neuf.fr>
2020-02-13 17:01:12 +0100

    Creating images folder

--- js/toDoList.js
+++ js/toDoList.js
@@ -1,5 +1,10 @@
 var images = []
-var url = function(images) {
+var url = function() {
     var url = new URL("https://wallpaperaccess.com/landscape-photography")
+    var parseUrl = url.searchParams.get("class");
 
+    https: //wallpaperaccess.com/download/landscape-photography-198417
+        console.log(parseUrl)
 }
+
+url()

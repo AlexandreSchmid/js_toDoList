147d4eda56585e871ee7138879a5b9b6899bf1cc
AlexandreSchmid <alexandre.schmid@neuf.fr>
2020-02-13 18:22:48 +0100

    final js part2

--- js/toDoList.js
+++ js/toDoList.js
@@ -1,17 +1,32 @@
+var todo = []
 var addToDo = document.getElementById('addToDo')
 var ul = document.getElementById('ul')
+var textValue = document.getElementById('textValue').value
+
 
 addToDo.addEventListener("click", function() {
-    var textValue = document.getElementById('textValue').value
     var li = document.createElement('li')
     var button = document.createElement('button')
 
-    var todo = []
+
     todo.push(textValue)
 
     li.setAttribute('class', 'list-group-item')
     li.innerHTML = textValue
-    button.innerHTML = 'X'
     button.setAttribute('class', 'btn btn-outline-danger')
+    button.setAttribute('id', 'thisButton')
+    button.innerHTML = 'X'
     ul.prepend(li, button)
 })
+
+function removeElement() {
+    try {
+        document.getElementById('thisButton').addEventListener("click", function() {
+            this.parentElement.remove()
+        })
+    } catch (TypeError) {
+
+    }
+
+}
+removeElement()

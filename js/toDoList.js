58ac8b33f057d8ce7b585d3b4990716ef44e1d1e
AlexandreSchmid <alexandre.schmid@neuf.fr>
2020-02-13 21:52:21 +0100

    JS IS DONE!!!

--- js/toDoList.js
+++ js/toDoList.js
@@ -6,18 +6,16 @@ var textValue = document.getElementById('textValue').value
 addToDo.addEventListener("click", function() {
     var li = document.createElement('li')
     var button = document.createElement('button')
+    var textNode = document.createTextNode(textValue)
     todo.push(textValue)
     li.setAttribute('class', 'list-group-item')
-    li.innerHTML = textValue
+    li.appendChild(textNode)
     button.setAttribute('class', 'badge badge-danger')
-    button.setAttribute('id', 'thisButton')
     button.innerHTML = 'Remove'
     li.appendChild(button)
     ul.prepend(li)
 })
 
-document.querySelector('li').addEventListener("click", function() {
-    if (document.querySelector('li > #thisButton')) {
-        this.parentNode.remove()
-    }
+document.querySelector('ul').addEventListener("click", function() {
+    ul.removeChild(ul.children.item(this.id))
 })

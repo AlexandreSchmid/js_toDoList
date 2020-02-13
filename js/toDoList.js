51eff1a7392db3de7288145a9202e44179ca28ad
AlexandreSchmid <alexandre.schmid@neuf.fr>
2020-02-13 21:52:49 +0100

    JS IS DONE 2!!!

--- js/toDoList.js
+++ js/toDoList.js
@@ -1,21 +1,26 @@
-var todo = []
+// Variables select the html elements
+var todo = [] // For later, will contain the other todos
 var addToDo = document.getElementById('addToDo')
 var ul = document.getElementById('ul')
 var textValue = document.getElementById('textValue').value
 
+// Triggered by a click on 'Add' button
 addToDo.addEventListener("click", function() {
-    var li = document.createElement('li')
-    var button = document.createElement('button')
-    var textNode = document.createTextNode(textValue)
-    todo.push(textValue)
-    li.setAttribute('class', 'list-group-item')
-    li.appendChild(textNode)
-    button.setAttribute('class', 'badge badge-danger')
-    button.innerHTML = 'Remove'
-    li.appendChild(button)
-    ul.prepend(li)
-})
-
+        todo.push(textValue) // Push into array (see below)
+            // Create elements for html structure
+        var li = document.createElement('li')
+        var button = document.createElement('button')
+        var textNode = document.createTextNode(textValue)
+            // Set attributes like class for css
+        li.setAttribute('class', 'list-group-item')
+        li.appendChild(textNode)
+        button.setAttribute('class', 'badge badge-danger')
+        button.innerHTML = 'Remove'
+            // Append button into ul child and add all new node to the top of the list
+        li.appendChild(button)
+        ul.prepend(li)
+    })
+    // When clicking on remove
 document.querySelector('ul').addEventListener("click", function() {
     ul.removeChild(ul.children.item(this.id))
 })

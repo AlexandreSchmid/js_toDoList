5e0f416c173e0b353a52753dc1ff7eea6152b3bc
AlexandreSchmid <alexandre.schmid@neuf.fr>
2020-02-19 21:26:45 +0100

    THE END

--- js/toDoList.js
+++ js/toDoList.js
@@ -1,33 +1,39 @@
 class Todolist {
-
+    // Setting variables for creating elements
     elts = ['li', 'p', 'button']
     ul = document.getElementById('ul')
-
+        // Setting only the text of each task
     constructor(textValue) {
-        this.textValue = textValue
-    }
-
-    createTask() {
-        // Create elements for html structure
-        var elt = []
-        for (var i = 0; i < this.elts.length; i++) {
-            elt.push(document.createElement(this.elts[i]))
+            this.textValue = textValue
         }
-        elt.push(document.createTextNode(this.textValue))
+        // Each task is created by putting text into elements created below
+    createTask() {
+            // Create elements for html structure
+            var elt = []
+            for (var i = 0; i < this.elts.length; i++) {
+                elt.push(document.createElement(this.elts[i]))
+            }
+            elt.push(document.createTextNode(this.textValue))
 
-        // Set attributes like class for css
-        elt[0].setAttribute('class', 'task')
-        elt[2].setAttribute('class', 'removeButton')
-        elt[2].innerHTML = 'Remove'
-            // Append button into ul child and add all new node to the top of the list
-        elt[1].appendChild(elt[3])
-        elt[0].appendChild(elt[1])
-        elt[0].appendChild(elt[2])
-        this.ul.prepend(elt[0])
+            // Set attributes like class for css
+            elt[0].setAttribute('class', 'task')
+            elt[2].setAttribute('class', 'removeButton')
+            elt[2].innerHTML = 'Remove'
+                // Append button into ul child and add all new node to the top of the list
+            elt[1].appendChild(elt[3])
+            elt[0].appendChild(elt[1])
+            elt[0].appendChild(elt[2])
+            this.ul.prepend(elt[0])
 
-        this.saveTasks()
-    }
-
+            this.saveTasks()
+        }
+        // Each task is removable individually, I use localstorage for persistence
+    removeItem() {
+            document.getElementById('ul').removeChild(document.getElementById('ul').children.item(this.id))
+            localStorage.setItem("todoList", "")
+            this.saveTasks()
+        }
+        // Each task is save with localStorage into an array
     saveTasks() {
         var tasks = []
         var items = document.querySelectorAll('li')
@@ -35,15 +41,6 @@ class Todolist {
             tasks.push(items[i].firstChild.textContent)
         }
         localStorage.setItem('todoList', tasks)
-    }
-
-    removeItem() {
-        document.querySelector('.removeButton').addEventListener("click", function() {
-            this.ul.removeChild(this.ul.children.item(this.id))
-            var items = localStorage.getItem("todoList").split(',')
-            items.pop(items[this.id])
-
-        })
     }
 }
 
@@ -54,12 +51,19 @@ window.addEventListener('load', function() {
             new Todolist(items[i]).createTask()
         }
     }
-    // When clicking on remove
-    new Todolist(null).removeItem()
-})
-
-// Triggered by a click on 'Add' button
-document.getElementById('addToDo').addEventListener("click", function() {
-    // Call to Todolist class function createTask()
-    new Todolist(document.getElementById('textValue').value).createTask()
+    // Remove tasks one by one
+    this.document.addEventListener('click', function(e) {
+            if (e.target.className == 'removeButton') {
+                new Todolist(null).removeItem()
+            }
+        })
+        // Triggered by a click on 'Add' button
+    document.getElementById('addToDo').addEventListener("click", function() {
+            // Call to Todolist class function createTask()
+            new Todolist(document.getElementById('textValue').value).createTask()
+        })
+        // Clear every tasks
+    this.document.getElementById('clearToDo').addEventListener("click", function() {
+        localStorage.setItem('todoList', '')
+    })
 })
